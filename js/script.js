b3ddb648d102b6b5df68589df35b87dd961c8e57
Rob Schuster <robert.is@icloud.com>
2023-03-19 20:47:05 -0400

    Collect Repos, and display from newest to oldest

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
   // Select the div with - the profile info section
 const overview = document.querySelector(".overview"); 
 const username = "rob17204";
+const repoList = document.querySelector(".repo-list");
 
 const gitHubProfile = async function (){
   const profileRequest = await fetch (`https://api.github.com/users/${username}`);
   
     // collect the profile api results
   const data = await profileRequest.json(); 
-  console.log(data);
+    //console.log(data);
 
   displayUser(data);
 };
@@ -21,7 +22,7 @@ const displayUser = function (data) {
   const div = document.createElement("div");
   div.classList.add("user-info");
 
-    // Display on page
+    // Display user info on page
   div.innerHTML = `
     <figure>
       <img src=${data.avatar_url} alt="user avatar" />
@@ -34,4 +35,23 @@ const displayUser = function (data) {
     </div>
   `;
   overview.append(div);
+  fetchRepos();
+  };
+
+    // Fetch repos
+  const fetchRepos = async function () {
+    const repoRequest = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await repoRequest.json();
+    console.log(repoData);
+    displayRepos(repoData);
+  };
+
+    // Display
+  const displayRepos = function (repos) {
+    for (const repo of repos) {
+      const repoItem = document.createElement("li");
+      repoItem.classList.add("repo");
+      repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+      repoList.append(repoItem);
+    }
   };
